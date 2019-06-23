15336ec448e0274150b4c8f31fa4863759aafa26
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-06-23 16:58:22 +0900

    create show.html.slim

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(user_params)
+  end
+
+
+
+
+
+  private
+    def user_params
+    end
 end
