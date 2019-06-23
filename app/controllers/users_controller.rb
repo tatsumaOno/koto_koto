4ec4bf0f90cf4fb7e9d3e82a97e07c815b8fc5c7
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-06-23 17:33:10 +0900

    create sign up not devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    binding.pry
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+      # redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
   def show
-    @user = User.find(user_params)
+    @user = User.find(params[:id])
   end
 
 
@@ -13,5 +24,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 end
