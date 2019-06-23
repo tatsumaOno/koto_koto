2ece6220ea6aebaa3e75db7ce501f584c588caee
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-06-23 18:25:42 +0900

    create session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'tops#index'
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login',to: 'sessions#new'
+  post '/login',to: 'sessions#create'
+  delete '/login',to: 'sessions#destroy'
 end
