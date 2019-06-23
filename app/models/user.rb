3674af6e7f98f869f77504c2de41e79fc04748ee
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-06-23 16:46:56 +0900

    create password not devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /([\w+\-.]+)@[a-z\d]+\.[a-z]{2,3}/i
 	validates :name, presence: true,length: {maximum: 50}
 	validates :email,presence: true,length: {maximum: 255},format: {with: VALID_EMAIL_REGEX},uniqueness: true
+	validates :password,presence: true,length: {minimum: 6}
 
-	has_sequre_password
+	has_secure_password
 	#セキュアなパスワードを作成
+	#password_digestカラム
+	#gem bcrypt
+	
 end
