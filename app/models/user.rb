356ac50ce3b88d3a175bc212f4e4b9c25e1741ac
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-06-23 16:32:28 +0900

    add email index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+	before_save {self.email = email.downcase}
+	VALID_EMAIL_REGEX = /([\w+\-.]+)@[a-z\d]+\.[a-z]{2,3}/i
 	validates :name, presence: true,length: {maximum: 50}
-	validates :email,presence: true,length: {maximum: 255}
+	validates :email,presence: true,length: {maximum: 255},format: {with: VALID_EMAIL_REGEX},uniqueness: true
+
+	has_sequre_password
+	#セキュアなパスワードを作成
 end
