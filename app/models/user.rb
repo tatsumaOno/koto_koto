357d3b8bef652af680b3c25ae3be485936db593c
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-06-24 15:52:54 +0900

    advance login cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,30 @@ class User < ApplicationRecord
 	#セキュアなパスワードを作成
 	#password_digestカラム
 	#gem bcrypt
-	
-	def User.digest(string)
+	attr_accessor :remember_token # 仮想の属性
+
+	def self.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(string,cost: cost)
+	end
+
+	#トークンを作成
+	def self.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#
+	def remember
+		self.remember_token = User.new_token #新しいトークンを作る
+		update_attribute(:remember_digest,User.digest(remember_token))
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest,nil)
 	end
 end
