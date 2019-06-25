8d2692b3a1637c1d192178d32c50c65804ff5e57
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-06-25 17:33:06 +0900

    add methods password_reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 	#セキュアなパスワードを作成
 	#password_digestカラム
 	#gem bcrypt
-	attr_accessor :remember_token,:activation_token # 仮想の属性
+	attr_accessor :remember_token,:activation_token,:reset_token # 仮想の属性
 	before_save :downcase_email
 	before_create :create_activation_digest
 
@@ -40,6 +40,21 @@ class User < ApplicationRecord
 
 	def forget
 		update_attribute(:remember_digest,nil)
+	end
+
+	#パスワードの再設定の属性を設定する
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at,Time.zone.now)
+	end
+
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
+	end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 
 private
