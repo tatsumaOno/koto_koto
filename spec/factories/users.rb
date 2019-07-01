964d1617e3f4cf30807a598a32f6d8502d76cb7e
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-07-02 00:14:00 +0900

    user model test

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+	factory :user do
+		name {"test10"}
+		nickname {"test10nick"}
+		email {"test10@gmail.com"}
+		password {"1234567"}
+		password_confirmation {"1234567"}
+	end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,60 @@
 require 'rails_helper' 
 describe User do
 	describe '#create' do
-	it "is invalid without a nickname" do
-	end 
+		context "名前" do
+			it "名前なし" do
+				user = build(:user,name: "")
+				user.valid?
+				expect(user.errors[:name]).to include("を入力してください")
+			end 
+			it "名前が50文字以上" do
+				user = build(:user,name: "a"*51)
+				user.valid?
+				expect(user.errors[:name]).to include("は50文字以内で入力してください")
+			end
+			it "名前が50文字以下" do
+				user = build(:user,name: "a"*50)
+				expect(user).to be_valid
+			end
+		end
+
+		context "メールアドレス" do
+			it "アドレスなし" do
+				user = build(:user,email: "")
+				user.valid?
+				expect(user.errors[:email]).to include("を入力してください")
+			end
+			it "アドレス100文字以上" do
+				user = build(:user,email: "a"*100+"@gmail.com")
+				user.valid?
+				expect(user.errors[:email]).to include("は100文字以内で入力してください")
+			end
+			it "アドレス100文字以下" do
+				user = build(:user,email: "a"*90+"@gmail.com")
+				expect(user).to be_valid
+			end
+			it "アドレス被り" do
+				user = create(:user)
+				another_user = build(:user,password: "9876543",password_confirmation: "9876543")
+				another_user.valid?
+				expect(another_user.errors[:email]).to include("はすでに存在します")
+			end
+		end
+		context "パスワード" do
+			it "パスワードなし" do
+				user = build(:user,password: "")
+				user.valid?
+				expect(user.errors[:password]).to include("を入力してください")
+			end
+			it "パスワード6文字以下" do
+				user = build(:user,password: "1234")
+				user.valid?
+				expect(user.errors[:password]).to include("は6文字以上で入力してください")
+			end
+			it "パスワード6文字以上" do
+				user = build(:user)
+				expect(user).to be_valid
+			end
+		end
 	end
 end
