9cd8e7f1223417bd7a8a534cf644ff851a29ee64
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-07-08 00:32:05 +0900

    add create methods

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,28 @@
 class ServicesController < ApplicationController
+  before_action :logged_in_user,only: [:edit,:update] 
+  before_action :correct_user,only: [:show,:edit,:update]
 
   def demand #悩み
     @service = Service.new()
   end
 
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      flash[:success] = "出品が完了しました"
+      redirect_to current_user
+    end
+  end
+
   def supply
     @service = Service.new()
   end
+
+
+
+private
+    def service_params
+      params.require(:service).permit(:image,:title,:detail,:price,:time,:work).merge(user_id: current_user.id)
+    end
+
 end
