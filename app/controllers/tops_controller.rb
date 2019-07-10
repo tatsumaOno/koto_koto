2307e399f2f9b373eda81846397772709a50cc89
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-07-10 15:52:43 +0900

    create card list of index

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,8 @@
 class TopsController < ApplicationController
   def index
+    @demands = Service.includes(:area).where(work: 0).order(created_at: :desc).limit(4)
+    @supplies = Service.includes(:area).where(work: 1).order(created_at: :desc).limit(4)
+    #仮配置
+    @category_service = Category.includes(:services).find(2)
   end
 end
