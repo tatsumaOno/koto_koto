1587719a79535a0c25f20c1351dc0d6231a3ea88
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-07-10 18:44:29 +0900

    validateを表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-	validates :detail,presence: true,length: {maximum: 100}
+	validates :detail,presence: true,length: {maximum: 50}
 
 	belongs_to :user
 	belongs_to :service
