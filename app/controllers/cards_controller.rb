10b637a2cffc6a23699ddfc61595f2f68a6a7db1
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-07-14 18:03:38 +0900

    カード作成機能　作成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,19 @@
 class CardsController < ApplicationController
+	require 'payjp'
+	include CardsHelper
+
 	def new
+	end
+
+	def create
+		create_customer(current_user)
+		@card = Card.new(user_id: current_user.id,customer_id: @customer.id,card_id: @customer.default_card)
+		if @card.save
+			redirect_to current_user
+			flash[:success] = "カード情報を登録しました"
+		else
+			render :new
+			flash[:danger] = "不正な情報です"
+		end
 	end
 end
--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,9 @@
 module CardsHelper
+	def create_customer(user)
+		Payjp.api_key = 'sk_test_a15dc8e443d70196461839ca'
+		@customer =	Payjp::Customer.create(
+						email: current_user.email,
+						card: params['payjp-token']
+					)
+	end
 end
