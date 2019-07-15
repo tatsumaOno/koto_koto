32755da120dd715fd601ed7972390407931ee2df
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-07-15 23:59:35 +0900

    購入機能　demand

--- app/controllers/services/agreements_controller.rb
+++ app/controllers/services/agreements_controller.rb
@@ -1,4 +1,14 @@
 class Services::AgreementsController < ApplicationController
 	def create
+		@service = Service.find(params[:service_id])
+		@card = Card.find_by(user_id: current_user.id)
+		if @card && @service.work == 'supply'
+			@customer = Card.set_customer(@card)
+			Card.create_charge(@customer,@service)
+			@service.progress = "契約中"
+			@service.save
+			redirect_to root_path
+			flash[:success] = "購入が完了しました"
+		end
 	end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -6,6 +6,15 @@ class Card < ApplicationRecord
 		Payjp::Customer.retrieve(card.customer_id)
 	end
 
+	def self.create_charge(customer,service)
+		Payjp.api_key = 'sk_test_a15dc8e443d70196461839ca'
+		Payjp::Charge.create(
+			:amount => service.price,
+			:customer => customer,
+			:currency => 'jpy',
+		)
+	end
+
 	def delete_payjp(customer)
 		self.destroy
 		customer.delete
