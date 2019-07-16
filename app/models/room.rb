7351280f83bf2a5da6982175842d469a2c464f5a
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-07-17 01:24:10 +0900

    add create_room method

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,11 @@
 class Room < ApplicationRecord
+	has_many :room_users
 	has_many :users,through: :room_users
+
+	def self.create_room(service,buy_user)
+		sell_user = service.user
+		@room = Room.new(name: service.title)
+		@room.users << [sell_user,buy_user]
+		@room.save
+	end
 end
