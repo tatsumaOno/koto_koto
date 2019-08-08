83a7191c1b747654b199c69e196605915a474071
tatsumaOno <tatsuma06.10.13@docomo.ne.jp>
2019-08-08 22:06:31 +0900

    add sass loading

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+	var loader = $('.wrapper');
+
+	//ページの読み込みが完了したらアニメーションを非表示
+	$(window).on('load',function(){
+		loader.fadeOut();
+	});
+
+	//ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+	setTimeout(function(){
+		loader.fadeOut();
+	},3000);
+});
